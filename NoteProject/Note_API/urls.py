b129db410a08f091b94c8877de03660cef8296fb
warriorwolfmj <sauravdodiya4@gmail.com>
2022-05-30 14:50:50 +0530

    first commit

--- NoteProject/Note_API/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-# from django.urls import path
-# from Note_API import views
-#
-# urlpatterns = [
-#
-#     path('Note', views.Note_API.as_view())
-#     path('NoteID)
-# ]
